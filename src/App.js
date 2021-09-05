22fd059ac1e6fc89189194c4714210eab6dc48bc
Beers15 <alexanderbeers15@gmail.com>
2021-09-05 16:00:09 -0700

    Add eslint, .gitignore. Add app structure and styled nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './Components/Header';
+import Main from './Components/Main';
+import About from './Components/About';
+import Profile from './Components/Profile';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/askchef">
+              <Main />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <h1>Home</h1>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Recipe and Meal Page here</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
