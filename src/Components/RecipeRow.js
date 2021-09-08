31c8de8e8cc8a80578277e8224f7e4b2ef71ab5c
AntoineCharette <antoineocharette@gmail.com>
2021-09-08 19:26:18 -0400

    modifies reciperow and attempts to add to db

--- src/Components/RecipeRow.js
+++ src/Components/RecipeRow.js
@@ -5,15 +5,13 @@ import RecipeCard from './RecipeCard';
 const RecipeRow = (props) => {
   return (
     <Row style={{ justifyContent: 'center' }}>
-      <Col style={{ display: 'flex', justifyContent: 'center' }}>
-        {props.recipes[0] && <RecipeCard isProfileCard={props.isProfileCardRow} recipe={props.recipes[0]} />}
-      </Col>
-      <Col style={{ display: 'flex', justifyContent: 'center' }}>
-        {props.recipes[1] && <RecipeCard isProfileCard={props.isProfileCardRow} recipe={props.recipes[1]} />}
-      </Col>
-      <Col style={{ display: 'flex', justifyContent: 'center' }}>
-        {props.recipes[2] && <RecipeCard isProfileCard={props.isProfileCardRow} recipe={props.recipes[2]} />}
-      </Col>
+      {props.recipes.map((recipe) => {
+        return (
+          <Col style={{ display: 'flex', justifyContent: 'center' }}>
+            <RecipeCard isProfileCard={props.isProfileCardRow} recipe={recipe} />
+          </Col>
+        );
+      })}
     </Row>
   );
 };
